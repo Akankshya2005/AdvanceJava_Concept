48c200ebe3570e883ef2861b4096c55d85c69b89
somjena <jenasoumitrapvt@gmail.com>
2026-07-13 15:06:48 +0530

    Step3,4,5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -11,18 +11,33 @@ public class Main {
         String USER = "root";
         String PASSWORD = "Rintu@1234";
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            DriverManager.getConnection(URL,USER,PASSWORD);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (SQLException e) {
+
+          Connection con = DriverManager.getConnection(URL,USER,PASSWORD);
+
+          PreparedStatement pr=con.prepareStatement("insert into student_data values (?,?,?)");
+            System.out.println("Enter id  : \n name :\n Domain :");
+            Scanner sc = new Scanner(System.in);
+
+
+          pr.setInt(1,sc.nextInt());
+          sc.nextLine();
+          pr.setString(2,sc.nextLine());
+          pr.setString(3,sc.nextLine());
+
+          //4th Step(Execute Query)
+           int rowsaffect = pr.executeUpdate();
+            System.out.println(rowsaffect);
+            if(rowsaffect>0){
+                System.out.println("Data inserted");
+            }
+            con.close();
+
+
+
+        } catch ( SQLException e) {
             throw new RuntimeException(e);
         }
 
-        System.out.println("Now we can Talk to Database");
-
-
-        System.out.println("Class will end at exact 5:30 so be patience");
 
     }
 }
