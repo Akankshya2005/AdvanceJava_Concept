00ec28cfba6020785dcac2100e3d8023ef31b21f
Akankshya2005 <tanujagiri75@gmail.com>
2026-07-20 12:10:59 +0530

    Updated Main.java with fetch query

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,7 +12,7 @@ public class Main {
         String PASSWORD = "akankshya2005";
         try {
           Connection con = DriverManager.getConnection(URL,USER,PASSWORD);
-
+/*
           PreparedStatement pr=con.prepareStatement("insert into student_data values (?,?,?)");
             System.out.println("Enter id  : \n name :\n Domain :");
             Scanner sc = new Scanner(System.in);
@@ -28,11 +28,21 @@ public class Main {
             System.out.println(rowsaffect);
             if(rowsaffect>0){
                 System.out.println("Data inserted");
+            }*/
+            PreparedStatement prs = con.prepareStatement("SELECT * FROM student_data");
+            ResultSet rs = prs.executeQuery();
+
+            System.out.println("\n--- Student Data ---");
+            while (rs.next()) {
+                int id = rs.getInt(1);       // column 1
+                String name = rs.getString(2); // column 2
+                String domain = rs.getString(3); // column 3
+
+                System.out.println("ID: " + id + ", Name: " + name + ", Domain: " + domain);
             }
+
+
             con.close();
-
-
-
         } catch ( SQLException e) {
             throw new RuntimeException(e);
         }
